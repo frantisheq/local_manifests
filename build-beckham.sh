27d632ab4e80ef788bb3d7c4944d846ddd0cf4fc
Jan Dula <frantisheq@gmail.com>
2019-06-05 12:37:50 +0200

    Update build-beckham.sh

--- build-beckham.sh
+++ build-beckham.sh
@@ -1,28 +1,48 @@
 #!/bin/bash
 
-lineageos=/home/frantisheq/Documents/android/lineageos-16.0
+lineageos=/media/android/lineageos
 
 basketbuild="frantisheq.basketbuild.com,**** basketbuild.com"
 webhost="frantisheq,**** files.000webhost.com"
+nas="frantisheq,**** 192.168.1.110"
 
 cd $lineageos
 
-export TMPDIR=/home/frantisheq/.tmpdir
+PATH=~/bin:$PATH
 
-repo sync --force-sync
+export TMPDIR=/home/frantisheq/.tmpdir
+export ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4096m"
+export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4096m"
+export OUT_DIR_COMMON_BASE=/media/android/out
+export USE_CCACHE=1
+
+
+#repo sync --force-sync
 source build/envsetup.sh
 
-#battery mod
+# MODS
+#repopick -f 206768
+#repopick -f 206769
+
+# Battery mod
 repopick -f 227142
 repopick 237140
 repopick 237141
 repopick 237142
 
+# JBL mod
+repopick 237143
+
+# Enable metadata for FDE
+#repopick 243744
+#repopick 243809
+
 breakfast beckham
 make installclean
 
 source build/envsetup.sh
-brunch beckham
+brunch beckham userdebug
 
-lftp -e "mrm beckham/lineage-16.0*beckham.zip*; ls beckham; mput -O beckham /home/frantisheq/Documents/android/lineageos-16.0/out/target/product/beckham/lineage-16.0*beckham.zip*; ls beckham; bye" -u $basketbuild
-lftp -e "mrm public_html/LineageOTA/builds/full/lineage-16.0*beckham.zip*; ls public_html/LineageOTA/builds/full; mput -O public_html/LineageOTA/builds/full /home/frantisheq/Documents/android/lineageos-16.0/out/target/product/beckham/lineage-16.0*beckham.zip*; ls public_html/LineageOTA/builds/full; bye" -u $webhost
+#lftp -e "mrm beckham/lineage-16.0*beckham.zip*; ls beckham; mput -O beckham /media/android/out/lineageos/target/product/beckham/lineage-16.0*beckham.zip*; ls beckham; bye" -u $basketbuild
+#lftp -e "set ftp:ssl-allow false; mrm web/LineageOTA/builds/full/lineage-16.0*beckham.zip*; ls web/LineageOTA/builds/full; mput -O web/LineageOTA/builds/full /media/android/out/lineageos/target/product/beckham/lineage-16.0*beckham.zip*; ls web/LineageOTA/builds/full; bye" -u $nas
+#lftp -e "set ftp:ssl-allow false; mrm public_html/LineageOTA/builds/full/lineage-16.0*beckham.zip*; ls public_html/LineageOTA/builds/full; mput -O public_html/LineageOTA/builds/full /media/android/out/lineageos/target/product/beckham/lineage-16.0*beckham.zip*; ls public_html/LineageOTA/builds/full; bye" -u $webhost
